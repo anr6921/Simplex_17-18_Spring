f639890dc372337c89e1b85ec56074ec63db3d36
Anna Rosenberg <anr6921@rit.edu>
2018-02-20 11:37:54 -0500

    merging albertos code

--- C11_TranslationAndScale/AppClass.cpp
+++ C11_TranslationAndScale/AppClass.cpp
@@ -25,10 +25,26 @@ void Application::Display(void)
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 	
+	vector3 v3Begin(-5.0f, 0.0f, 0.0f);
+	vector3 v3End(5.0f, 0.0f, 0.0f);
+
+	static DWORD dStartTime = GetTickCount();
+	DWORD dTimer = GetTickCount() - dStartTime;
+	float fTimer = dTimer / 1000.0f;
+
+	static float fPercentage = 0.0f;
+	vector3 v3Current = glm::lerp(v3Begin, v3End, 0.0f);
+	fPercentage += 0.01f;
+	
+
+	m_m4Model = glm::translate(IDENTITY_M4, v3Current);
+
+	m_pMesh->Render(m4Projection, m4View, m_m4Model);
+	/*/
 	matrix4 m4Scale = glm::scale(IDENTITY_M4, vector3(2.0f,2.0f,2.0f));
 	static float value = 0.0f;
 	matrix4 m4Translate = glm::translate(IDENTITY_M4, vector3(value, 2.0f, 3.0f));
-	value += 0.01f;
+	value += 0.01f;*/
 
 	//matrix4 m4Model = m4Translate * m4Scale;
 	matrix4 m4Model = m4Scale * m4Translate;
@@ -43,6 +59,25 @@ void Application::Display(void)
 
 	//clear the render list
 	m_pMeshMngr->ClearRenderList();
+
+	//copied from AppClassGUI
+#pragma region Debugging Information
+	//Print info on the screen
+	uint nEmptyLines = 21;
+	for (uint i = 0; i < nEmptyLines; ++i)
+		m_pMeshMngr->PrintLine("");//Add a line on top
+								   //m_pMeshMngr->Print("						");
+	m_pMeshMngr->PrintLine(m_pSystem->GetAppName(), C_YELLOW);
+	m_pMeshMngr->Print("Percentage: ", C_BLUE);
+
+
+	//m_pMeshMngr->Print("						");
+	m_pMeshMngr->Print("RenderCalls: ");//Add a line on top
+	m_pMeshMngr->PrintLine(std::to_string(m_uRenderCallCount), C_YELLOW);
+
+	//m_pMeshMngr->Print("						");
+	m_pMeshMngr->Print("FPS:");
+	m_pMeshMngr->Print(std::to_string(m_pSystem->GetFPS()), C_RED);
 	
 	//draw gui
 	DrawGUI();
